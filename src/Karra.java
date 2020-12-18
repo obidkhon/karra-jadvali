ebb1790dbf5eadc3c1e8bf629f564055b64e174b
obidkhon <obid.sharipov.80@mail.ru>
2020-12-18 23:57:56 +0500

    karra jadvali

---
+++ src/Karra.java
@@ -0,0 +1,11 @@
+public class Karra {
+    public static void main(String[] args) {
+        for (int k=1; k<=10;k++) {
+            for (int i = 1; i <= 10; i++) {
+                System.out.printf(" %3d", i*k);
+            }
+            System.out.println();
+        }
+
+    }
+}
